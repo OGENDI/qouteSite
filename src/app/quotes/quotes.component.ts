720db701035fc6a35ac8f5555742f3af701e819d
OGENDI <demokonge17@gmail.com>
2022-01-09 22:11:09 +0300

    Array updates

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,7 +8,7 @@ import { MyQuotes } from '../my-quotes';
 })
 export class QuotesComponent implements OnInit {
   quotes:MyQuotes[]=[
-    new MyQuotes(1,"Keep it Moving","Derick","Manely",0,0,new Date(2022,0,1)),
+    new MyQuotes(1,"The greatest glory in living lies not in never falling, but in rising every time we fall. ","Derick","-Nelson Mandela",0,0,new Date(2022,0,1)),
     new MyQuotes(2,"Yes I can", "Oyama", "Oloo",0,0,new Date(2020,0,18)),
     new MyQuotes(3,"We deliver the best", "Oyoo", "Musa",0,0,new Date(2010,10,18)),
   ]
