1d40a5aae79eed4e58353e47487b27dc4757ea96
OGENDI <demokonge17@gmail.com>
2022-01-09 21:23:21 +0300

    Added quotes array

--- src/app/my-quotes.ts
+++ src/app/my-quotes.ts
@@ -2,7 +2,7 @@ export class MyQuotes {
     showDescription:boolean;
     constructor(public id:number, public quote:string, public username:string,
         public author:string, public upVote:number, public downVote:number, public dateSubmitted:Date){
-            this.showDescription == false;
+            this.showDescription = false;
 
     }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MyQuotes } from '../my-quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes:MyQuotes[]=[
+    new MyQuotes(1,"Keep it Moving","Derick","Manely",0,0,new Date(2022,0,1)),
+    new MyQuotes(2,"Yes I can", "Oyama", "Oloo",0,0,new Date(2020,0,18)),
+    new MyQuotes(3,"We deliver the best", "Oyoo", "Musa",0,0,new Date(2010,10,18)),
+  ]
   constructor() { }
 
   ngOnInit(): void {
