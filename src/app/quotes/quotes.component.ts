dcb99746c97a9c754235956bd4c24c0c5e6f4d35
OGENDI <demokonge17@gmail.com>
2022-01-09 23:20:19 +0300

    added methos to add a new quote to the already exisiting ones/ array

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,7 +11,19 @@ export class QuotesComponent implements OnInit {
     new MyQuotes(1,"The greatest glory in living lies not in never falling, but in rising every time we fall. ","Derick","-Nelson Mandela",0,0,new Date(2022,0,1)),
     new MyQuotes(2,"If life were predictable it would cease to be life, and be without flavor.", "Oyama", "-Eleanor ",0,0,new Date(2020,0,18)),
     new MyQuotes(3,"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success", "Oyoo", "James Cameron",0,0,new Date(2010,10,18)),
-  ]
+  ];
+
+  addNewQuote(newQuote:MyQuotes){
+    var quotesLength = this.quotes.length;
+    newQuote.id = quotesLength+1;
+    newQuote.dateSubmitted =new Date( newQuote.dateSubmitted);
+    this.quotes.push(newQuote);
+
+  }
+
+
+
+
   constructor() { }
 
   ngOnInit(): void {
