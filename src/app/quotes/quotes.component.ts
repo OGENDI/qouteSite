df01fe6415e7c417dcf660d5caa32657d5d2e0cd
OGENDI <demokonge17@gmail.com>
2022-01-09 22:17:07 +0300

    quotes updates

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,8 +9,8 @@ import { MyQuotes } from '../my-quotes';
 export class QuotesComponent implements OnInit {
   quotes:MyQuotes[]=[
     new MyQuotes(1,"The greatest glory in living lies not in never falling, but in rising every time we fall. ","Derick","-Nelson Mandela",0,0,new Date(2022,0,1)),
-    new MyQuotes(2,"Yes I can", "Oyama", "Oloo",0,0,new Date(2020,0,18)),
-    new MyQuotes(3,"We deliver the best", "Oyoo", "Musa",0,0,new Date(2010,10,18)),
+    new MyQuotes(2,"If life were predictable it would cease to be life, and be without flavor.", "Oyama", "-Eleanor ",0,0,new Date(2020,0,18)),
+    new MyQuotes(3,"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success", "Oyoo", "James Cameron",0,0,new Date(2010,10,18)),
   ]
   constructor() { }
 
