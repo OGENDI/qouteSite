abcd84516d749b063406b5ffbc433f825d87059c
OGENDI <demokonge17@gmail.com>
2022-01-10 10:40:57 +0300

    Added delete methods( vote, de-vpde and delete)

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MyQuotes } from '../my-quotes';
 
 @Component({
@@ -7,6 +7,10 @@ import { MyQuotes } from '../my-quotes';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  @Input() quote!:MyQuotes;
+  @Output() disliked = new EventEmitter<boolean>();
+
+
   quotes:MyQuotes[]=[
     new MyQuotes(1,"The greatest glory in living lies not in never falling, but in rising every time we fall. ","Derick","-Nelson Mandela",0,0,new Date(2022,0,1)),
     new MyQuotes(2,"If life were predictable it would cease to be life, and be without flavor.", "Oyama", "-Eleanor ",0,0,new Date(2020,0,18)),
@@ -18,7 +22,28 @@ export class QuotesComponent implements OnInit {
     newQuote.id = quotesLength+1;
     newQuote.dateSubmitted =new Date( newQuote.dateSubmitted);
     this.quotes.push(newQuote);
+  }
 
+  delete(notgood:boolean){
+    this.disliked.emit(notgood);
+
+  }
+  deletequote(notgood:boolean, index:number){
+    if(notgood){
+      var todelete = confirm( " This QUote will be deleted");
+      if(todelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
+
+  vote(id:number){
+    
+    this.quotes[id].upVote +=1
+  }
+  de_vote(id:number){
+    
+    this.quotes[id].downVote +=1
   }
 
 
